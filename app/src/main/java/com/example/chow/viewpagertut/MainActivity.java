e80ccc4ae697e831e10d4203ada1bd8a66536cd7
Chi Chow <kyroschow@gmail.com>
2019-09-24 12:49:04 -0700

    Migrate to AndroidX

--- app/src/main/java/com/example/chow/viewpagertut/MainActivity.java
+++ app/src/main/java/com/example/chow/viewpagertut/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.chow.viewpagertut;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/chow/viewpagertut/ViewPagerAdapter.java
+++ app/src/main/java/com/example/chow/viewpagertut/ViewPagerAdapter.java
@@ -1,8 +1,8 @@
 package com.example.chow.viewpagertut;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 /**
  * Created by per6 on 1/10/18.
